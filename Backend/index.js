01110ca91703b8e333147ff6e1c1ebb5205f428c
Kk-0010 <148414514+Kk-0010@users.noreply.github.com>
2025-01-02 12:46:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("/api/v1/auth", authRoutes);
+app.use("https://intern-task-kk-frontend.vercel.app", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 // app.use("/api/v1/product", productRoutes);
 
