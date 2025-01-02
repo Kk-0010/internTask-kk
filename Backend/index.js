c427203440e9ed9d09f658350008e22ba226e801
Kk-0010 <148414514+Kk-0010@users.noreply.github.com>
2025-01-02 13:15:52 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,12 +18,17 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(cors(
+  origin: ["https://intern-task-kk-frontend.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true;
+}
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("https://intern-task-kk-frontend.vercel.app", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 // app.use("/api/v1/product", productRoutes);
 
